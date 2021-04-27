a7a8bcfdf97d97505fc7d0934bd26f75323056f7
voldeck-fledge <mark@thefledge.com>
2021-04-27 08:59:08 -0400

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { xdai, dai, eth } from '@burner-wallet/assets';
+import { ERC20Asset } from '@burner-wallet/assets';
 import BurnerCore from '@burner-wallet/core';
 import { InjectedSigner, LocalSigner } from '@burner-wallet/core/signers';
 import { InfuraGateway, InjectedGateway, XDaiGateway, } from '@burner-wallet/core/gateways';
-import Exchange, { Uniswap, XDaiBridge } from '@burner-wallet/exchange';
+//import Exchange, { Uniswap, XDaiBridge } from '@burner-wallet/exchange';
 import ModernUI from '@burner-wallet/modern-ui';
+//import {Biconomy} from '@biconomy/mexa';
+
+const fldg = new ERC20Asset({
+  id: 'fldg',
+  name: 'TEST',
+  network: '100',
+  address: '0xE58F4bfAC26736b7EE4272809a3F47adEfe59BAB',
+  icon: 'https://thefledge.community/custom/images/fldg_icon.png',
+});
 
 const core = new BurnerCore({
   signers: [new InjectedSigner(), new LocalSigner()],
@@ -15,18 +24,16 @@ const core = new BurnerCore({
     new XDaiGateway(),
   ],
   // TODO use Sai
-  assets: [xdai, dai, eth],
+  assets: [fldg],
 });
 
-const exchange = new Exchange({
-  pairs: [new XDaiBridge(), new Uniswap('dai')],
-});
+//const biconomy = new Biconomy(new InfuraGateway(process.env.REACT_APP_INFURA_KEY),{apiKey: 'gX7qIsQoe.7f06c33d-1499-4d2b-8c1d-339494939d8d', debug: true});
+//web3 = new Web3(biconomy);
 
 const BurnerWallet = () =>
   <ModernUI
-    title="Basic Wallet"
+    title="FLDG Wallet"
     core={core}
-    plugins={[exchange]}
   />
 
 
